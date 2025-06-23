ae945c0e4ffc1334c90d22dfce1f1f354604fc5f
VariaSlu <varvaraslugina@gmail.com>
2025-06-23 22:04:21 +0200

    seetup mongodb

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 import cors from "cors"
 import express from "express"
+import listEndpoints from "express-list-endpoints";
+//import thoughtsData from "./thoughts.json" assert { type: 'json' };
+
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log('Connected to MongoDB!'))
+  .catch((err) => console.error('DB error:', err));
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -11,10 +22,31 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+//set up what we need for data base from Jennies video
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/something"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!")
+  res.json({
+    message: 'Welcome to the Happy Thoughts API!',
+    endpoints: listEndpoints(app)
+  })
 })
+
+app.get('/thoughts', (req, res) => {
+  res.json(thoughtsData);
+});
+
+app.get('/thoughts/:id', (req, res) => {
+  const thought = thoughtsData.find(t => t.id === +req.params.id);
+  if (!thought) {
+    res.status(404).json({ error: 'Thought not found' });
+  } else {
+    res.json(thought);
+  }
+});
 
 // Start the server
 app.listen(port, () => {
